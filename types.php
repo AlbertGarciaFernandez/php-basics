df91f0acb5b8ee618e34d2492a32498d90ba4b5f
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2021-12-14 14:52:23 +0100

    types ok

---
+++ types.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Absolute value returns 3.2
+echo(abs(-3.2));
+echo "<br>";
+// Rounded value returns 2
+echo(round(1.70));
+echo "<br>";
+// Highest value returns 240
+echo(max(0, 240, 60, 15, -45, -350));
+echo "<br>";
+// Lowest value returns -350
+echo(min(0, 240, 60, 15, -45, -350));
+echo "<br>";
+// Random number
+echo(rand());
+echo "<br>";
+
+?>
+
+</body>
+</html>
